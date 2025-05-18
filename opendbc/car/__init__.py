62479e52b81e3c050ee012ed93700cc56b7dd880
mike8643 <nguyen95465@yahoo.com>
2025-05-17 23:57:23 -0400

    crc8_pedal

--- opendbc/car/__init__.py
+++ opendbc/car/__init__.py
@@ -219,6 +219,19 @@ def make_tester_present_msg(addr, bus, subaddr=None, suppress_response=False):
   dat.extend([0x0] * (8 - len(dat)))
   return CanData(addr, bytes(dat), bus)
 
+def crc8_pedal(data):
+  crc = 0xFF    # standard init value
+  poly = 0xD5   # standard crc8: x8+x7+x6+x4+x2+1
+  size = len(data)
+  for i in range(size - 1, -1, -1):
+    crc ^= data[i]
+    for _ in range(8):
+      if ((crc & 0x80) != 0):
+        crc = ((crc << 1) ^ poly) & 0xFF
+      else:
+        crc <<= 1
+  return crc
+
 def create_gas_interceptor_command(packer, gas_amount, idx):
   # Common gas pedal msg generator
   enable = gas_amount > 0.001
